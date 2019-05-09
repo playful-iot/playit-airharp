e8e265f9b82b4abdb1ee5a559966504b882c54fb
lillaki <ronjafaltin@gmail.com>
2019-05-09 16:42:21 +0200

    Added sound in index.js 'node-aplay' was installed to play music through node js

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const SerialPort = require('serialport')
 const Regex = require('@serialport/parser-regex')
 const port = new SerialPort('/dev/ttyUSB0')
 
+var Sound = require('node-aplay');
+
 // Parse the raw data
 const parser = port.pipe(new Regex({ regex: /[\r\n]+/ }))
 parser.on('data', function(data) {
     if(data < 'R015') {
+
+	new Sound('piano-e.wav').play();
 
         console.log('Data: ' + data + ' is smaller than R015');
     }
