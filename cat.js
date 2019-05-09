aad692e51c3bdfd167d8c9b8b0898b13e59e1990
Ronja Faltin <ronjafaltin@gmail.com>
2019-05-09 17:15:58 +0200

    Create a .js program with a cat sound

---
+++ cat.js
@@ -0,0 +1,16 @@
+const SerialPort = require('serialport')
+const Regex = require('@serialport/parser-regex')
+const port = new SerialPort('/dev/ttyUSB0')
+
+var Sound = require('node-aplay');
+
+// Parse the raw data
+const parser = port.pipe(new Regex({ regex: /[\r\n]+/ }))
+parser.on('data', function(data) {
+    if(data < 'R015') {
+
+	new Sound('cat.mp3').play();
+
+        console.log('Data: ' + data + ' is smaller than R015');
+    }
+});
